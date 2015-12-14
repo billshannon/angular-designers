7cc3f33353ad21275c04461337da0a8865c19a21
Bill Shannon <bill@billshannon.com>
2015-12-14 15:20:30 -0700

    add click events

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 angular.module('myApp', ['ngMaterial'])
     .controller('MainCtrl', function () {
         //var imagePath = 'assets/images/angular-logo.svg'
-        var imagePath = 'http://www.noao.edu/image_gallery/images/d2/ngc2237_200.jpg'
+        var imagePath = 'http://www.noao.edu/image_gallery/images/d2/ngc2237_200.jpg';
 
-        this.items = [
-            {
+        this.setCurrentItem = function(item) {
+            this.currentItem = item;
+        };
+
+        this.items = [{
                 label: 'Item One',
                 description: 'This is the first item',
                 price: '100',
@@ -24,6 +27,5 @@ angular.module('myApp', ['ngMaterial'])
                 price: '300',
                 url: 'http://angularjs.org',
                 avatar: imagePath
-            }
-        ]
+            }]
     });
