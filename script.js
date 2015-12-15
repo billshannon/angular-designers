842c589f962a2047a1f2454309638c04db7e3d84
Bill Shannon <bill@billshannon.com>
2015-12-15 15:44:57 -0700

    add select options

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 angular.module('myApp', ['ngMaterial', 'ngMessages'])
+    .controller('OptionsCtrl', function () {
+        this.options = [
+            {label: 'Option 01', value: 1},
+            {label: 'Option 02', value: 2},
+            {label: 'Option 03', value: 3},
+        ];
+
+    })
+
     .controller('MainCtrl', function () {
         //var imagePath = 'assets/images/angular-logo.svg'
         var imagePath = 'http://www.noao.edu/image_gallery/images/d2/ngc2237_200.jpg';
 
-        this.setCurrentItem = function(item) {
+        this.setCurrentItem = function (item) {
             this.currentItem = item;
         };
 
-        this.submitItem = function(item) {
+        this.submitItem = function (item) {
             this.submittedMessage = 'Success! ' + item.label + ' was submmited!';
         }
 
         this.items = [{
             id: 1,
-                label: 'Item One',
-                description: 'This is the first item',
-                price: 100,
-                url: 'http://angularjs.org',
-                avatar: imagePath
-            },
+            label: 'Item One',
+            description: 'This is the first item',
+            price: 100,
+            url: 'http://angularjs.org',
+            avatar: imagePath
+        },
             {
                 id: 2,
                 label: 'Item Two',
