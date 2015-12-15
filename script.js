2c132bfa9b24762bd46a17d8817de152f388ba74
Bill Shannon <bill@billshannon.com>
2015-12-15 16:07:54 -0700

    add mouse enter and leave events

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 angular.module('myApp', ['ngMaterial', 'ngMessages'])
-    .controller('OptionsCtrl', function () {
-        this.options = [
-            {label: 'Option 01', value: 1},
-            {label: 'Option 02', value: 2},
-            {label: 'Option 03', value: 3},
-        ];
-
-    })
-
     .controller('MainCtrl', function () {
         //var imagePath = 'assets/images/angular-logo.svg'
         var imagePath = 'http://www.noao.edu/image_gallery/images/d2/ngc2237_200.jpg';
@@ -18,7 +9,19 @@ angular.module('myApp', ['ngMaterial', 'ngMessages'])
 
         this.submitItem = function (item) {
             this.submittedMessage = 'Success! ' + item.label + ' was submmited!';
-        }
+        };
+
+        this.discount = 0;
+        this.showDiscount = function() {
+            this.discount++;
+            this.discountMessage = 'Hooray! Your discount is ' + this.discount + '%'
+        };
+
+        this.removeDiscount = function () {
+            this.discountMessage = null
+        };
+
+
 
         this.items = [{
             id: 1,
